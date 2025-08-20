e58bee4cbaa7a97dd1d5ee87ea6a508f82801e99
VSobolenko <v.sobolenko.vs@gmail.com>
2025-08-20 22:20:38 +0300

    pre installed settings

---
+++ AmongUsRoomSettings/wwwroot/AccessScripts.js
@@ -0,0 +1,38 @@
+﻿function setAccessCookie(key, minutes) {
+    const d = new Date();
+    d.setTime(d.getTime() + (minutes*60*1000));
+    const expires = "expires=" + d.toUTCString();
+    document.cookie = "accessKey=" + key + ";" + expires + ";path=/";
+}
+
+function checkAccessCookie() {
+    const name = "accessKey=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i].trim();
+        if (c.indexOf(name) === 0) return true;
+    }
+    return false;
+}
+
+async function checkKey() {
+    const key = document.getElementById("keyInput").value;
+
+    const response = await fetch("/api/access/check", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({key})
+    });
+
+    const data = await response.json();
+
+    if (response.ok && data.success) {
+        document.getElementById("mainUI").style.display = "block";
+        document.getElementById("accessBlock").style.display = "none";
+    } else {
+        alert("Неверный ключ! Спроси у @gexetr какой ключ");
+        document.getElementById("mainUI").style.display = "none";
+    }
+}
+    
